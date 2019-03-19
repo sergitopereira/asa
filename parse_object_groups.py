79f755b11a10d9a62b5bf8962ef896dda22686c2
serg4250 <sergio.pereira@rackspace.com>
2019-03-19 15:34:04 -0500

    Added method to parse names and IPs

--- parse_object_groups.py
+++ parse_object_groups.py
@@ -2,6 +2,7 @@ import re
 import json
 import logging
 
+
 class ParseObjects(object):
     """
     This is a class in order to parse names and IPs of Cisco ASA running configuration Object-Groups
@@ -10,10 +11,9 @@ class ParseObjects(object):
     def __init__(self, running_config):
         self.running_config = running_config.split('\n')
 
-    def parsenames(self):
-
+    def parse_names(self):
         """
-        Tis is method to parse name ans IPS.
+        This method parses name and IPS, returning a dictionary key=name, value=ip.
         Example:
         name 192.168.1.100 Office1
         name 8.8.8.8 dnsGoogle
@@ -22,7 +22,47 @@ class ParseObjects(object):
         regex = r'name\s((\d+\.?)+)\s(.*)'
         nameip = {}
         for line in self.running_config:
-            result = re.search(regex,line)
+            result = re.search(regex, line)
             if result:
-                nameip[result.group(3)]=result.group(1)
+                nameip[result.group(3)] = result.group(1)
         return nameip
+
+    def parse_object_networks(self):
+        """
+        This method parses object networks, returning a dictionary key=name, value=ip.
+        Example:
+        object network obj-192.168.100.10
+         host 192.168.100.10
+        object network www.sergio.com
+         fqdn www.sergio.com
+        object network OfficeNet
+         subnet 192.168.0.0/24
+        object network net1
+         range 192.168.1.0 192.168.1.10
+        :return dic
+        """
+        flag_host_fqdn_range=False
+        network_host = {}
+        network_fqdn = {}
+        network_range = {}
+        network={}
+        regex_name = r'object\snetwork\s(.*)'
+        regex_value = r'(host|fqdn|range)\s(.*)'
+        for line in  self.running_config:
+            if line.startswith("object "):
+                flag_host_fqdn_range = True
+                name = re.search(regex_name, line).group(1)
+            if flag_host_fqdn_range:
+                if "host" in line:
+                    ip = re.search(regex_value, line).group(2)
+                    network_host[name] = ip
+                if "fqdn" in line:
+                    fqdn = re.search(regex_value, line).group(2)
+                    network_fqdn[name] = fqdn
+            if "range" in line:
+                    range = re.search(regex_value, line).group(2)
+                    network_range[name] = range
+        network['host'] = network_host
+        network['fqdn'] = network_fqdn
+        network['range'] = network_range
+        return network
